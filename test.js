5a89844c892fe3abcbb40f19ea8b5fd37cc12e52
Bella <dark.shadow2019@yahoo.com>
2023-08-28 13:32:59 +0300

    looking for the bug

--- test.js
+++ test.js
@@ -1,27 +1,34 @@
-let doc = [...document.childNodes]
+// let doc = [...document.childNodes]
 
-function nodeChecker(node) {
-    if (node.offsetWidth > 375) {
-        console.log("found it")
-        console.log(node)
-        return node
+// function nodeChecker(node) {
+//     if (node.offsetWidth > 375) {
+//         console.log("found it")
+//         console.log(node)
+//         return node
+//     }
+//     if (node.hasChildNodes()) {
+//         // check the parent first   
+//         let grandkids = [...node.childNodes]
+//         grandkids.forEach((kid) => {
+//             return nodeChecker(kid)
+//         })
+//     } 
+// }
+
+
+// doc.forEach((node) => {
+//     let faultyNode = nodeChecker(node)
+//     console.dir(faultyNode)
+// })
+
+
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
     }
-    if (node.hasChildNodes()) {
-        // check the parent first   
-        let grandkids = [...node.childNodes]
-        grandkids.forEach((kid) => {
-            return nodeChecker(kid)
-        })
-    } 
-}
-
-
-doc.forEach((node) => {
-    let faultyNode = nodeChecker(node)
-    console.dir(faultyNode)
-})
-
-
-// let d = document.querySelector(".story-text-box").childNodes
-// console.dir(d[1])
-
+  }
+);
