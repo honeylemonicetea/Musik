a7f921cd57730019f08219e24300be47bf079d07
Bella <dark.shadow2019@yahoo.com>
2023-08-30 14:27:24 +0300

    fix mobile

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ let scheduleBtn = document.getElementById("schedule-btn")
 let speakersDrop = document.querySelector(".speakers-drop")
 let scheduleDrop = document.querySelector(".schedule-drop")
 
-speakersBtn.onclick = () =>{
-    if (speakersDrop.classList.contains("speakers-drop-hidden")){
+speakersBtn.onclick = () => {
+    if (speakersDrop.classList.contains("speakers-drop-hidden")) {
         speakersDrop.classList.remove("speakers-drop-hidden")
-    } else{
+    } else {
         speakersDrop.classList.add("speakers-drop-hidden")
     }
 
 }
 
 
-scheduleBtn.onclick = () =>{
-    if (scheduleDrop.classList.contains("schedule-drop-hidden")){
+scheduleBtn.onclick = () => {
+    if (scheduleDrop.classList.contains("schedule-drop-hidden")) {
         scheduleDrop.classList.remove("schedule-drop-hidden")
-    } else{
+    } else {
         scheduleDrop.classList.add("schedule-drop-hidden")
     }
 }
@@ -24,20 +24,20 @@ scheduleBtn.onclick = () =>{
 
 // EVENT SCHEDULE
 let days = document.querySelectorAll(".day")
-days.forEach((e)=>{
+days.forEach((e) => {
     e.addEventListener("click", daySwitch)
     console.log(e)
 })
 
-function daySwitch () {
+function daySwitch() {
     let target = event.target.parentElement
     console.log(target.classList)
-    if (target.classList.contains("day")){
+    if (target.classList.contains("day")) {
         target.classList.add("active")
         // select the rest and remove the active class
         let inactiveDays = document.querySelectorAll(".day")
-        inactiveDays.forEach((e)=>{
-            if (e != target){
+        inactiveDays.forEach((e) => {
+            if (e != target) {
                 e.classList.remove("active")
             }
         })
@@ -49,28 +49,32 @@ function daySwitch () {
 
 let testimonials = document.querySelectorAll(".testimonial")
 
+// DISABLE ON MOBILE DEVICES
 
-testimonials.forEach((e)=>{
-    e.onclick = function(e){
+testimonials.forEach((e) => {
+    e.onclick = function (e) {
+
         let card = e.target.parentElement.parentElement
-     
+
         let cards = document.querySelectorAll(".testimonial")
         let otherCards = []
-        cards.forEach((item)=>{
-            if (item!=card){
+        cards.forEach((item) => {
+            if (item != card) {
                 otherCards.push(item)
             }
         })
         console.log(otherCards)
-        card.style.order = 2
-        card.classList.add("test-act")
-        card.classList.remove("test-inact")
-        otherCards[0].style.order = 1
-        otherCards[0].classList.remove("test-act")
-        otherCards[0].classList.add("test-inact")
-        otherCards[1].style.order = 3
-        otherCards[1].classList.remove("test-act")
-        otherCards[1].classList.add("test-inact")
+        if (window.innerWidth > 1000) {
+            card.style.order = 2
+            card.classList.add("test-act")
+            card.classList.remove("test-inact")
+            otherCards[0].style.order = 1
+            otherCards[0].classList.remove("test-act")
+            otherCards[0].classList.add("test-inact")
+            otherCards[1].style.order = 3
+            otherCards[1].classList.remove("test-act")
+            otherCards[1].classList.add("test-inact")
+        }
     }
 })
 
@@ -87,15 +91,15 @@ let swipedLeft = false
 let swipedRight = false
 let offset = 0
 
-leftBtn.onclick = function(){
-    if (offset>-380){
+leftBtn.onclick = function () {
+    if (offset > -380) {
         offset -= 380
         photos.style.transform = `translateX(${offset}px)`
     }
 }
-rightBtn.onclick = function(){
-    if (offset<380){
-        offset+=380
+rightBtn.onclick = function () {
+    if (offset < 380) {
+        offset += 380
         photos.style.transform = `translateX(${offset}px)`
     }
 }
